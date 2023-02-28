4a1b20fc08b479d23654d3556f2cbe1ebf9569de
alfredomayorga <115187281+alfredomayorga@users.noreply.github.com>
2023-02-28 08:54:25 -0600

    build: cypress setup for testing

---
+++ cypress.config.ts
@@ -0,0 +1,9 @@
+export default {
+  e2e: {
+    setupNodeEvents(on: any, config: any) {
+      // implement node event listeners here
+    },
+  },
+};
+
+export {};
---
+++ cypress/e2e/app-loaded.cy.ts
@@ -0,0 +1,7 @@
+describe('App loaded', () => {
+  it('loaded', () => {
+    cy.visit('http://localhost:3000')
+  })
+})
+
+export {}
